6d02adf6800a33ab9f36b353a94707895ab58b82
Brian Frisbie <bfrizb@github.com>
2020-03-17 20:38:30 -0700

    Checkpoint

--- src/index.js
+++ src/index.js
@@ -1,53 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Game from './app';
+import Users from './wordList';
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      word: null,
-      difficulty: null,
-      wordList: null,
-    };
-    this.state.wordList = this.readWordList();
-  }
-
-  readWordList() {
-    // TODO
-  }
-
-  getNewWord(difficulty) {
-    this.setState({ difficulty });
-    if (difficulty) this.setState({ word: JSON.stringify(new Date()) });
-  }
-
-  render() {
-    if (this.state.difficulty) {
-      return (
-        <>
-          <div>
-            Level {this.state.difficulty} word here! ({JSON.stringify(new Date())})
-          </div>
-          <button onClick={() => this.getNewWord(this.state.difficulty)}>Next Word</button>
-          <br />
-          <button onClick={() => this.setState({ difficulty: null })}>Main Menu</button>
-          <br />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <button onClick={() => this.getNewWord(1)}>Easy Word</button>
-          <br />
-          <button onClick={() => this.getNewWord(2)}>Medium Word</button>
-          <br />
-          <button onClick={() => this.getNewWord(3)}>Hard Word</button>
-          <br />
-        </>
-      );
-    }
-  }
-}
-
-ReactDOM.render(<Game />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={Game} />
+      <Route path="/words" component={Users} />
+    </div>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById('root'));
