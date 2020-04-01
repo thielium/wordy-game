73a485e1d8812a621da9d95e2e5cc38d8c4c69e6
Brian Frisbie <bfrizb@github.com>
2020-03-31 22:32:38 -0700

    Better structuring repo

--- backend/src/routes/words.ts
+++ backend/src/routes/words.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 var router = express.Router();
 
 router.get('/', (__: express.Request, res: express.Response) => {
-  const data = fs.readFileSync(__dirname + '/../../aux/word_list.yaml', 'utf8');
+  const data = fs.readFileSync(__dirname + '/../../../aux/word_list.yaml', 'utf8');
   res.send(data);
 });
 
